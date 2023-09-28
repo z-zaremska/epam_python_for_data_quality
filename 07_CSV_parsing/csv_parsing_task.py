c909ba9228484ce9bc51f55303a3e409341ddc6c
z-zaremska <zuza.zaremska@gmail.com>
2023-09-28 15:17:05 +0200

    Generated csv file with letter count.

--- 07_CSV_parsing/csv_parsing_task.py
+++ 07_CSV_parsing/csv_parsing_task.py
@@ -24,3 +24,35 @@ with open('word_count.csv', 'w', encoding='utf-8', newline='') as f:
     file_writer = csv.writer(f)
     for k, v in words_collection.items():
         file_writer.writerow([k, v])
+
+
+# Letter count
+letters_collection = {}
+for char in text:
+    if char.isalpha():
+        uppercase = 0
+        if char == char.upper():
+            uppercase = 1
+
+        if letters_collection.get(char.lower()):
+            letters_collection[char.lower()] = {
+                'total': letters_collection[char.lower()]['total'] + 1,
+                'uppercase': letters_collection[char.lower()]['uppercase'] + uppercase
+            }
+        else:
+            letters_collection[char.lower()] = {
+                'total': 1,
+                'uppercase': uppercase
+            }
+
+with open('letter_count.csv', 'w', encoding='utf-8', newline='') as f:
+    headers = ['letter', 'count_all', 'count_uppercase', 'percentage']
+    file_writer = csv.DictWriter(f, headers)
+    file_writer.writeheader()
+    for k, v in letters_collection.items():
+        file_writer.writerow({
+            'letter': k,
+            'count_all': letters_collection[k]['total'],
+            'count_uppercase': letters_collection[k]['uppercase'],
+            'percentage': letters_collection[k]['uppercase']/letters_collection[k]['total']
+        })
